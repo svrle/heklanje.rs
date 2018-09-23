6d62c91245664f38bd2e686cd53ee750fc0dabce
Nenad Grgic <svrle@Grgas-MacBook-Pro.local>
2018-09-23 12:13:58 +0200

    Crochet form and twig

--- src/Controller/CrochetController.php
+++ src/Controller/CrochetController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Crochet;
+use App\Form\CrochetType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CrochetController extends AbstractController
@@ -15,5 +18,30 @@ class CrochetController extends AbstractController
         return $this->render('crochet/index.html.twig', [
             'controller_name' => 'CrochetController',
         ]);
+    }
+
+    /**
+     * @Route("/user/crochet/new", name="user_newCrochet")
+     */
+    public function newAction(Request $request) {
+
+        $crochet = new Crochet();
+        $form = $this->createForm(CrochetType::class, $crochet);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($crochet);
+            $em->flush();
+
+            return $this->redirectToRoute('user_newCrochet');
+        }
+
+        return $this->render('crochet/new.html.twig', [
+            'crochetForm' => $form->createView()
+        ]);
+
+
     }
 }
