97a29ec39c7f23c78838ad46843f3e6fbadfa48e
Nenad Grgic <svrle@Grgas-MacBook-Pro.local>
2018-10-12 22:16:04 +0200

    without fosuserbundle

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,26 +5,21 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you require will output into a single css file (app.css in this case)
-require('../css/app.css');
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-var $ = require('jquery');
-var greet = require('./greet');
-
-$(document).ready(function() {
-    $('body').prepend('<h1>'+greet('john')+'</h1>');
-});
-//console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
-//const $ = require('jquery');
+const $ = require('jquery');
 // JS is equivalent to the normal "bootstrap" package
 // no need to set this to a variable, just require it
-//require('bootstrap');
+require('bootstrap');
 
 // or you can include specific pieces
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
-//$(document).ready(function() {
-//    $('[data-toggle="popover"]').popover();
-//});
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
+// require the JavaScript
+require('bootstrap-star-rating');
+// require 2 CSS files needed
+require('bootstrap-star-rating/css/star-rating.css');
+require('bootstrap-star-rating/themes/krajee-svg/theme.css');
